7caa1fffd84a154e794c168f893253456e44da24
Cristian Dulcey <cristian@loducode.com>
2020-07-19 22:32:32 -0500

    Cambio url del paquete

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
 setup(
     name='loducode_utils',
     packages=['loducode_utils'],  # this must be the same as the name above
-    version='0.0.0',
+    version='0.0.1',
     description='Basic components for the development of loducode s.a.s.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Cristian Dulcey',
     author_email='cristian@loducode.com',
-    url='https://github.com/UnTalDulcey/loducode_maytapi',  # use the URL to the github repo
-    download_url='https://github.com/UnTalDulcey/loducode_maytapi/tarball/0.1',
+    url='https://github.com/UnTalDulcey/loducode_utils',  # use the URL to the github repo
+    download_url='https://github.com/UnTalDulcey/loducode_utils/tarball/0.1',
     keywords=['utils', 'loducode_utils', 'loducode'],
     classifiers=[],
     install_requires=[i.strip() for i in open("requirements.txt").readlines()],
